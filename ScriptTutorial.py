407d86f07f98ff6b19841ad10e1c8de7da236fbc
alyson <alyson.basilio@gmail.com>
2017-08-02 21:10:50 -0300

    Simple Recommender System

--- ScriptTutorial.py
+++ ScriptTutorial.py
@@ -1,21 +1,47 @@
 import pandas as pd
+import numpy as np
+#import matplotlib.pyplot as plt
 
-# pass in column names for each CSV and read them using pandas.
-# Column names available in the readme file
+#column headers for the dataset
+data_cols = ['user id','movie id','rating','timestamp']
+item_cols = ['movie id','movie title','release date',
+'video release date','IMDb URL','unknown','Action',
+'Adventure','Animation','Childrens','Comedy','Crime',
+'Documentary','Drama','Fantasy','Film-Noir','Horror',
+'Musical','Mystery','Romance ','Sci-Fi','Thriller',
+'War' ,'Western']
+user_cols = ['user id','age','gender','occupation',
+'zip code']
 
-#Reading users file:
-u_cols = ['user_id', 'age', 'sex', 'occupation', 'zip_code']
-users = pd.read_csv('ml-100k/u.user', sep='|', names=u_cols,
- encoding='latin-1')
-print(u_cols[1])
+#importing the data files onto dataframes
+users = pd.read_csv('ml-100k/u.user', sep='|',
+names=user_cols, encoding='latin-1')
+item = pd.read_csv('ml-100k/u.item', sep='|',
+names=item_cols, encoding='latin-1')
+data = pd.read_csv('ml-100k/u.data', sep='\t',
+names=data_cols, encoding='latin-1')
 
-#Reading ratings file:
-r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
-ratings = pd.read_csv('ml-100k/u.data', sep='\t', names=r_cols,
- encoding='latin-1')
+#Create one data frame from the three
+dataset = pd.merge(pd.merge(item, data),users)
+print(dataset.head())
 
-#Reading items file:
-i_cols = ['movie id', 'movie title' ,'release date','video release date', 'IMDb URL', 'unknown', 'Action', 'Adventure',
- 'Animation', 'Children\'s', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy',
- 'Film-Noir', 'Horror', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
-items = pd.read_csv('ml-100k/u.item', sep='|', names=i_cols, encoding='latin-1')
+ratings_total = dataset.groupby('movie title').size()
+print(ratings_total.head())
+
+ratings_mean = (dataset.groupby('movie title'))['movie title','rating'].mean()
+print(ratings_mean.head())
+
+#modify the dataframes so that we can merge the two
+ratings_total = pd.DataFrame({'movie title':ratings_total.index,
+'total ratings': ratings_total.values})
+ratings_mean['movie title'] = ratings_mean.index
+
+final = pd.merge(ratings_mean, ratings_total).sort_values(by = 'total ratings',
+ascending= False)
+print(final.head())
+
+print(final.describe())
+
+final = final[:300].sort_values(by = 'rating',
+ascending = False)
+print(final.head())
