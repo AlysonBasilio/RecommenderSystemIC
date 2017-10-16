20d3d293a3fe0ea08b88fdb5fc5b36aa8db961d8
Alyson <alyson.basilio@delfosim.com>
2017-10-16 16:21:26 -0200

    começar escolher atributo

--- NaiveBayesClassifier/Decision Tree.py
+++ NaiveBayesClassifier/Decision Tree.py
@@ -7,29 +7,30 @@ user_cols = ['user id','gender','age','occupation','zip code']
 
 #importing the data files onto dataframes
 users = pd.read_csv('ml-1m/users.dat', sep='::',
-names=user_cols, encoding='latin-1')
+names=user_cols, encoding='latin-1', engine='python')
 # print(users.head())
 
 movies = pd.read_csv('ml-1m/movies.dat', sep='::',
-names=movies_cols, encoding='latin-1')
+names=movies_cols, encoding='latin-1', engine='python')
 # print(movies.head())
 
 ratings = pd.read_csv('ml-1m/ratings.dat', sep='::',
-names=ratings_cols, encoding='latin-1')
+names=ratings_cols, encoding='latin-1', engine='python')
 # print(ratings.head())
 
 #Create one data frame from the three
-dataset1 = pd.merge(pd.merge(movies[['movie id', 'genres']], ratings[['user id','movie id','rating']]),users[['user id','gender','age','occupation']])
+dataset1 = pd.merge(pd.merge(movies[['movie id', 'genres']], ratings[['user id','movie id','rating']]),
+                    users[['user id','gender','age','occupation']])
 print("Head of Dataset1")
 print(dataset1.head())
 
 #Create one data frame of how many times a movie was rated
-ratings_total1 = dataset1.groupby('movie title').size()
+ratings_total1 = dataset1.groupby('movie id').size()
 print("Head of Total Number of Ratings per Each Movie")
 print(ratings_total1.head())
 
 #Create one data frame with the mean rate for each movie
-ratings_mean1 = (dataset1.groupby('movie title'))['movie title','rating'].mean()
+ratings_mean1 = (dataset1.groupby('movie id'))['movie id','rating'].mean()
 print("Mean Rate")
 print(ratings_mean1.head())
 
@@ -42,6 +43,7 @@ class Node():
         self.filhos[valor] =node
 
 def escolherAtributo(atributos, exemplos):
+    
     return None
 
 def getDecisionTree(exemplos, atributos, padrao):
@@ -49,28 +51,37 @@ def getDecisionTree(exemplos, atributos, padrao):
     if len(exemplos)==0:
         return padrao
     else:
-        m = exemplos['ratings'][0]
+        m = int(exemplos.iloc[0,3])
         b = True
-        for i in range(len(exemplos['ratings'])-1):
-            if m != exemplos['ratings'][i+1]:
+        for i in exemplos.iloc[:,3]:
+            if m != int(i):
                 b = False
                 break
         if b:
             return m
         else:
             if len(atributos)==0:
-                ratings_total = exemplos.groupby('ratings').size().sort_values(by='ratings', ascending=False)
-                return ratings_total['ratings'][0]
+                m = (exemplos.groupby('rating'))['rating'].size()
+                m = m.sort_values(ascending=False)
+                return m.index[0]
             else:
                 melhor = escolherAtributo(atributos,exemplos)
                 tree = Node(melhor)
-                m = ratings_total = exemplos.groupby('ratings').size().sort_values(by='ratings', ascending=False)['ratings'][0]
+                m = (exemplos.groupby('rating'))['rating'].size()
+                m = m.sort_values(ascending=False)
+                m = m.index[0]
+                print('m = ',m)
                 for v in exemplos[melhor]:
                     e = exemplos[exemplos[melhor]==v]
                     sub_arvore = getDecisionTree(e,atributos-melhor,m)
-                    tree.incluirFilhos(vi,sub_arvore)
-
+                    tree.incluirFilhos(v,sub_arvore)
     return tree
+
+dataset1.sample(frac=1).reset_index(drop=True)
+dataset_train = dataset1[:int(len(dataset1)*2/3)]
+dataset_test = dataset1[int(len(dataset1)*2/3):]
+
+print(getDecisionTree(dataset_train,['genres','age','gender','occupation'],1))
 
 # #modify the dataframes so that we can merge the two
 # ratings_total1 = pd.DataFrame({'movie title':ratings_total1.index,
